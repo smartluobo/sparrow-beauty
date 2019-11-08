6bcd596b83fa3de87857a5997d2c1da1bd2a8612
lwx487239 <576596150@qq.com>
2019-11-08 16:59:00 +0800

    计算购物车价格

--- src/main/java/com/chaomeis/sparrowbeauty/api/service/calculate/CalculateService.java
+++ src/main/java/com/chaomeis/sparrowbeauty/api/service/calculate/CalculateService.java
@@ -38,9 +38,9 @@ public class CalculateService {
     @Resource
     private  TbCartMapper tbCartMapper;
     /**
-     * 结算订单价格
+     * 结算商品价格
      * @param goodsParamVO 创建订单的参数
-     * @return 返回订单价格对象 返回对象待定义
+     * @return 返回订单价格对象
      */
     public CalculateReturnVo calculateCartGoodsPrice(GoodsParamVO goodsParamVO){
         // 商品总支付金额
@@ -59,7 +59,7 @@ public class CalculateService {
         // 活动优惠价格
         BigDecimal activityPrice = this.calculateActivityPrice(goodsParamVO.getGoods().getGoodsId(), goodsParamVO.getGoods().getGoodsCount());
         // 券优惠价格c
-        BigDecimal couponsPrice = this.calculateCouponsPrice(goodsPayAmount, goodsParamVO.getUserCouponsId(), goodsParamVO.getOpenId());
+        BigDecimal couponsPrice = this.calculateCouponsPrice(originalPrice, goodsParamVO.getUserCouponsId(), goodsParamVO.getOpenId());
         // sku价格
         BigDecimal skuPrice = new BigDecimal("0");
         if(!CollectionUtils.isEmpty(goods.getSkuDetailIdList())) {
@@ -74,7 +74,7 @@ public class CalculateService {
         // 支付价格
         goodsPayAmount = originalPrice.subtract(activityPrice).subtract(couponsPrice).add(skuPrice);
         // 总金额
-        goodsTotalAmount = originalPrice;
+        goodsTotalAmount = originalPrice.add(skuPrice);
         // 优惠金额
         goodsReduceAmount = activityPrice.add(couponsPrice);
 
@@ -86,95 +86,66 @@ public class CalculateService {
         // 商品金额
         return calculateReturn;
     }
-    public CalculateReturnVo calculateCartOrderPrice(CartOrderParamVo cartOrderParamVo){
-        List<String> cartIdList = Arrays.asList(cartOrderParamVo.getCartItemIds().split(","));;
-        // 商品总支付金额
-        BigDecimal goodsPayAmount = new BigDecimal("0");
-        // 商品总金额
-        BigDecimal goodsTotalAmount = new BigDecimal("0");
-        // 优惠券金额
-        BigDecimal goodsReduceAmount = new BigDecimal("0");
 
+    /**
+     * 计算购物车价格
+     * @param cartOrderParamVo
+     * @return
+     */
+    public CalculateReturnVo calculateCartOrderPrice(CartOrderParamVo cartOrderParamVo){
+        // 商品总支付金额
+        BigDecimal orderGoodsPayAmount = new BigDecimal("0");
+        // 商品总金额
+        BigDecimal orderGoodsTotalAmount = new BigDecimal("0");
+        // 优惠券金额
+        BigDecimal orderGoodsReduceAmount = new BigDecimal("0");
+
+        List<String> cartIdList = Arrays.asList(cartOrderParamVo.getCartItemIds().split(","));;
         for (String cartId : cartIdList) {
+            // 商品总支付金额
+            BigDecimal goodsPayAmount = new BigDecimal("0");
+            // 商品总金额
+            BigDecimal goodsTotalAmount = new BigDecimal("0");
+            // 优惠券金额
+            BigDecimal goodsReduceAmount = new BigDecimal("0");
             TbCart tbCart = tbCartMapper.selectByPrimaryKey(Integer.valueOf(cartId)); // 购物车信息
             TbGoods tbGoods = tbGoodsMapper.selectByPrimaryKey(tbCart.getGoodsId());
             String goodsPrice = tbGoods.getGoodsPrice();
             BigDecimal bidGoodsPrice = new BigDecimal(goodsPrice);
             BigDecimal goodsCount = new BigDecimal(tbCart.getGoodsCount()); // 商品数量
             // 商品原价
-            goodsPayAmount = bidGoodsPrice.multiply(goodsCount);
-            goodsTotalAmount = goodsPayAmount;
-
-            // 计算活动价格
-            TbActivity tbActivity= tbActivityMapper.selectValidActivityy();
-            if (null != tbActivity) {
-                if ("0".equals(tbActivity.getActivityType())) { // 全场折扣
-                    BigDecimal ratio = new BigDecimal(tbActivity.getActivityRatio());
-                    BigDecimal ratioyAmount = goodsPayAmount.multiply(ratio); // 折扣价格
-                    goodsReduceAmount = goodsPayAmount.subtract(ratioyAmount); // 减免金额
-                    goodsPayAmount = ratioyAmount; // 支付金额
-                } else if ("1".equals(tbActivity.getActivityType())) { // 限时活动
-                    // 限时活动时，查询商品是否在当前活动中
-                    TbActivityGoods record = new TbActivityGoods();
-                    record.setGoodsId(tbGoods.getId());
-                    record.setActivityId(tbActivity.getId());
-                    TbActivityGoods tbActivityGoods = tbActivityGoodsMapper.selectActivityGoods(record);
-                    if (0 == tbActivityGoods.getPriceType()) { // 指定价格活动
-                        BigDecimal activitAmount =  new BigDecimal(tbActivityGoods.getActivitPrice()).multiply(goodsCount); // 活动价格
-                        goodsReduceAmount = goodsPayAmount.subtract(activitAmount); // 减免金额
-                        goodsPayAmount = activitAmount; // 支付金额
-                    } else if (1 == tbActivityGoods.getPriceType()) { // 折扣价格活动
-                        BigDecimal ratio = new BigDecimal(tbActivityGoods.getActivityRatio());
-                        BigDecimal ratioyAmount = goodsPayAmount.multiply(ratio); // 折扣价格
-                        goodsReduceAmount =  goodsPayAmount.subtract(ratioyAmount); // 减免金额
-                        goodsPayAmount = ratioyAmount; // 支付金额
-                    }
+            BigDecimal originalPrice = bidGoodsPrice.multiply(goodsCount);
+            // 活动优惠价格
+            BigDecimal activityPrice = this.calculateActivityPrice(tbCart.getGoodsId(), tbCart.getGoodsCount());
+            // 券优惠价格
+            BigDecimal couponsPrice = this.calculateCouponsPrice(originalPrice, cartOrderParamVo.getUserCouponsId(), cartOrderParamVo.getOpenId());
+            // sku价格
+            BigDecimal skuPrice = new BigDecimal("0");
+            List<String> skuIdList = Arrays.asList(tbCart.getSkuDetailIds().split(","));;
+            if(!CollectionUtils.isEmpty(skuIdList)) {
+                BigDecimal bigSkuDetailPrice= new BigDecimal ("0");
+                for (String skuTypeId: skuIdList) {
+                    // sku价格
+                    bigSkuDetailPrice = calculateSkuprice(Integer.valueOf(skuTypeId));
+                    bigSkuDetailPrice = bigSkuDetailPrice.add(bigSkuDetailPrice);
                 }
+                skuPrice = bigSkuDetailPrice;
             }
+            // 支付价格
+            goodsPayAmount = originalPrice.subtract(activityPrice).subtract(couponsPrice).add(skuPrice);
+            // 总金额
+            goodsTotalAmount = originalPrice.add(skuPrice);
+            // 优惠金额
+            goodsReduceAmount = activityPrice.add(couponsPrice);
+            orderGoodsPayAmount = orderGoodsPayAmount.add(goodsPayAmount);
+            orderGoodsTotalAmount = orderGoodsTotalAmount.add(goodsTotalAmount);
+            orderGoodsReduceAmount = orderGoodsReduceAmount.add(goodsReduceAmount);
         }
 
-        // 代金券
-        if (cartOrderParamVo.getUserCouponsId() > 0) {
-            TbUserCoupons record = new TbUserCoupons();
-            record.setOpenId(cartOrderParamVo.getOpenId());
-            record.setId(cartOrderParamVo.getUserCouponsId());
-            TbUserCoupons userCoupons = tbUserCouponsMapper.selectUserCoupons(record);
-            if (1 == userCoupons.getCouponsType()) { // 折扣券
-                BigDecimal ratio = new BigDecimal(userCoupons.getCouponsRatio());
-                BigDecimal ratioyAmount = goodsPayAmount.multiply(ratio); // 折扣后价格
-                goodsReduceAmount =  goodsPayAmount.subtract(ratioyAmount).add(goodsReduceAmount); // 减免金额
-                goodsPayAmount = ratioyAmount; // 支付金额
-            } else if (2 ==userCoupons.getCouponsType()) { // 满减券
-                BigDecimal consumeAmount = new BigDecimal(userCoupons.getConsumeAmount()); // 消费金额
-                int compareValue = goodsPayAmount.compareTo(consumeAmount);
-                if (compareValue >= 0) { // 商品金额大于券消费金额满足活动
-                    BigDecimal reduceAmount = new BigDecimal(userCoupons.getReduceAmount());
-                    goodsPayAmount = goodsPayAmount.subtract(reduceAmount);
-                    goodsReduceAmount = reduceAmount.add(goodsReduceAmount);
-                }
-            }
-        }
-        // sku价格
-        for (String cartId : cartIdList) {
-            TbCart tbCart = tbCartMapper.selectByPrimaryKey(Integer.valueOf(cartId)); // 购物车信息
-            String skuDeList = tbCart.getSkuDetailIds();
-            List<String> skuId = new ArrayList<>();
-            if (!StringUtils.isEmpty(skuDeList)) {
-                skuId = Arrays.asList(skuDeList.split(","));
-            }
-            for (String skuTypeId: skuId) {
-                // 商品sku
-                TbSkuDetail skuList = tbSkuDetailMapper.selectByPrimaryKey(Integer.valueOf(skuTypeId));
-                BigDecimal goodsCount = new BigDecimal(tbCart.getGoodsCount());
-                BigDecimal bigSkuDetailPrice = new BigDecimal(skuList.getSkuDetailPrice()).multiply(goodsCount);
-                goodsPayAmount = goodsPayAmount.add(bigSkuDetailPrice); // 支付金额
-                goodsTotalAmount = goodsTotalAmount.add(bigSkuDetailPrice); // 总价格
-            }
-        }
         CalculateReturnVo calculateReturn = new CalculateReturnVo();
-        calculateReturn.setOrderTotalAmount(goodsTotalAmount.toString()); // 总金额
-        calculateReturn.setOrderPayAmount(goodsPayAmount.toString()); // 支付金额
-        calculateReturn.setOrderReduceAmount(goodsReduceAmount.toString()); // 减免金额
+        calculateReturn.setOrderTotalAmount(orderGoodsTotalAmount.toString()); // 总金额
+        calculateReturn.setOrderPayAmount(orderGoodsPayAmount.toString()); // 支付金额
+        calculateReturn.setOrderReduceAmount(orderGoodsReduceAmount.toString()); // 减免金额
         // 订单邮费
         // 商品金额
         return calculateReturn;
@@ -243,7 +214,7 @@ public class CalculateService {
                 int compareValue = goodsPayAmount.compareTo(consumeAmount);
                 if (compareValue >= 0) { // 商品金额大于券消费金额满足活动
                     BigDecimal reduceAmount = new BigDecimal(userCoupons.getReduceAmount());
-                    couponsReduceAmount = goodsPayAmount.subtract(reduceAmount);
+                    couponsReduceAmount = reduceAmount;
                 }
             }
         }
